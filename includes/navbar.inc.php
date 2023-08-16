9dd3aba99b66a1e91eb461236031cad385d92250
liamnicola <liamnicola@hotmail.com>
2023-08-16 13:41:01 +0100

    working navbar

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -1,22 +1,8 @@
-<h3>Navigation Bar Tabs</h3>
-
-	<ul class="nav nav-pills">
-	  <li class="nav-item">
-	    <a class="nav-link active" href="index.php">Home</a>
-	  </li>
-<li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
-        <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li class="nav-item"><a href="#">Page 1-1</a></li>
-          <li class="nav-item"><a href="#">Page 1-2</a></li>
-          <li class="nav-item"><a href="#">Page 1-3</a></li>
-        </ul>
-      </li>
-	  <li class="nav-item">
-	    <a class="nav-link" href="about-us.php">About Us</a>
-	  </li>
-	  <li class="nav-item">
-	    <a class="nav-link" href="contact-us.php">Contact</a>
-	  </li>
-	</ul>
+<ul class="nav nav-pills">
+    <li class="nav-item">
+        <a class="nav-link active" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+        <a href="leagues.php">View Leagues</a>
+    </li>
+</ul>
