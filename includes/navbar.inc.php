521270c9ab7f034a74ac62985c9812dea81d654a
liamnicola <liamnicola@hotmail.com>
2023-08-19 13:54:19 +0100

    table css

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -1,11 +1,13 @@
 <style>
 <?php include './css/navbar.css' ?>
 </style>
-<ul class="nav nav-pills">
-    <li class="nav-item">
-        <a class="nav-link active" href="index.php">Home</a>
-    </li>
-    <li class="nav-item">
-        <a href="leagues.php">View Leagues</a>
-    </li>
-</ul>
+<div>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link active" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="leagues.php">View Leagues</a>
+        </li>
+    </ul>
+</div>
--- leagues.php
+++ leagues.php
@@ -22,32 +22,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    <?php include './css/table.css' ?>
+</style>
 </head>
 <body>
     <?php include("includes/navbar.inc.php");?>
     <h1>Existing Leagues</h1>
-
+    <div>
+        <table>
+            <tr>
+                <th>League ID</th>
+                <th>League Name</th>
+            </tr>
     <?php
        if (empty($results)) {
         echo "<p> No Existing Leagues </p>";
        } 
        else {
         foreach($results as $row){
-            echo "<div>";
-            echo "<table>";
-            echo "<tr>";
-            echo "<th> League ID </th>";
-            echo "<th> League Name </th>";
-            echo "</tr>";
             echo "<tr>";
             echo "<td>" . htmlspecialchars($row["id"]) . "</td>";
             echo "<td>" . htmlspecialchars($row["name"]) . "</td>";
             echo "</tr>";
-            echo "</table>";
-            echo "</div>";
         }
        }
     ?>
-    
+    </table>
+    </div>
 </body>
 </html>
